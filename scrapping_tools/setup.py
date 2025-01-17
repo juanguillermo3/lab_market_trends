e92c36f97684485b01864e8ca6a6544678da4790
juanguillermo3 <135280741+juanguillermo3@users.noreply.github.com>
2025-01-17 18:13:42 -0500

    Update setup.py

--- scrapping_tools/setup.py
+++ scrapping_tools/setup.py
@@ -1,22 +1,22 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='scrapping_tools',
-    version='0.1.0',
-    author='Juan Guillermo',
-    author_email='juanosio838@gmail.com',
-    description='A library for Selenium-based web navigation.',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    url='https://github.com/juanguillermo3/lab_market_trends/new/main/scrapping_tools',
-    packages=find_packages(),
-    install_requires=[
-        'selenium>=4.0.0',
-    ],
+    name="scrapping-tools",  # Name of the package
+    version="0.1.0",
+    author="Juan Guillermo",
+    author_email="juanosio838@gmail.com",
+    description="Web scraping tools using Selenium.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/juanguillermo3/lab_market_trends/tree/main/scrapping_tools",
+    packages=find_packages(),  # This ensures all Python files inside are included
     classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
-    python_requires='>=3.7',
+    python_requires=">=3.6",
+    install_requires=[
+        "selenium>=4.0.0",  # Your package dependencies
+    ],
 )
